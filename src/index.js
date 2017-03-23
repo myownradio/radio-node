b2db40aeaa81002cc3d44701052ac0fc322a2087
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2017-03-23 12:53:17 +0200

    Send independent track to all users

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 // @flow
 
 import express from 'express';
+import ffmpeg from 'fluent-ffmpeg';
+import * as fs from 'fs';
 
 const startServer = (port: number) => {
   const app: express$Application = express();
@@ -12,6 +14,18 @@ const startServer = (port: number) => {
   app.get('/ping', (req: express$Request, res: express$Response) => {
     res.header('Content-Type', 'text/plain');
     res.send('pong');
+  });
+
+  app.get('/test', (req: express$Request, res: express$Response) => {
+    ffmpeg(fs.createReadStream('__tests__/__fixtures__/demo.mp3'))
+      .outputFormat('mp3')
+      .on('end', () => {
+        console.log('file has been converted successfully');
+      })
+      .on('error', (err) => {
+        console.log(`an error happened: ${err.message}`);
+      })
+      .pipe(res, { end: true });
   });
 
   app.get('/channel/:channelId', (req: express$Request, res: express$Response) => {
