cd82a50d3a4dc26b527ddd33ea2e3856d45ee704
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2017-03-25 23:47:28 +0200

    Refactoring

--- src/stream/stream-utils.js
+++ src/stream/stream-utils.js
@@ -2,7 +2,7 @@
 
 import { Writable } from 'stream';
 
-export const pass = (target: stream$Writable) => new Writable({
+export const pass = (target: stream$Writable): stream$Writable => new Writable({
   write(chunk: any, encoding: any, cb: any): boolean {
     return target.write(chunk, encoding, cb);
   },
