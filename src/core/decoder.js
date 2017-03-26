7a98b695d16d1743b5e529cfc979260a61b68fb4
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2017-03-26 11:14:11 +0300

    Refactoring decoder.js

--- src/core/decoder.js
+++ src/core/decoder.js
@@ -7,22 +7,19 @@ import { pass } from '../utils/stream-utils';
 export const DECODER_CHANNELS = 2;
 export const DECODER_FREQUENCY = 44100;
 export const DECODER_FORMAT = 's16le';
-export const DECODER_CODEC = 'pcm_s16le';
+export const AUDIO_CODEC = 'pcm_s16le';
 
-export default (sourceUrl: string, offsetMillis: number, targetStream: stream$Writable,
-  nextCallback: () => void) => {
-  ffmpeg(sourceUrl)
-    .native()
-    .seekInput(offsetMillis / 1000)
-    .audioCodec(DECODER_CODEC)
-    .audioChannels(DECODER_CHANNELS)
-    .audioFrequency(DECODER_FREQUENCY)
-    .outputFormat(DECODER_FORMAT)
-    .audioFilter('afade=t=in:st=0:d=1')
-    .on('end', () => nextCallback())
-    .on('error', (error) => {
-      console.error(error);
-      setTimeout(nextCallback, 1000);
-    })
-    .pipe(pass(targetStream));
-};
+export default (sourceUrl: string, offsetMillis: number, writer: stream$Writable): Promise<void> =>
+  new Promise((resolve, reject) => {
+    ffmpeg(sourceUrl)
+      .native()
+      .seekInput(offsetMillis / 1000)
+      .audioCodec(AUDIO_CODEC)
+      .audioChannels(DECODER_CHANNELS)
+      .audioFrequency(DECODER_FREQUENCY)
+      .outputFormat(DECODER_FORMAT)
+      .audioFilter('afade=t=in:st=0:d=1')
+      .on('end', () => resolve())
+      .on('error', () => reject())
+      .pipe(pass(writer));
+  });
