3596bb473dbbebc35cb4221942e7599d3194c329
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2017-03-31 17:06:16 +0300

    Add logging to container

--- src/core/container.js
+++ src/core/container.js
@@ -1,5 +1,7 @@
 // @flow
 
+import winston from 'winston';
+
 import Player from './player';
 
 type PlayerMap = { [key: string]: Player };
@@ -10,10 +12,13 @@ export default class Container {
 
   constructor(backend: string) {
     this.backend = backend;
+    winston.log('info', 'Initialized players container');
   }
 
   createOrGetPlayer(channelId: string): Player {
+    winston.log('info', 'Looking for player', { channelId });
     if (!(channelId in this.players)) {
+      winston.log('info', 'There is no player so we create it', { channelId });
       this.players[channelId] = this._createPlayer(channelId);
     }
     return this.players[channelId];
@@ -26,6 +31,7 @@ export default class Container {
   }
 
   _removePlayer(channelId: string) {
+    winston.log('info', 'Removing player from container', { channelId });
     delete this.players[channelId];
   }
 }
