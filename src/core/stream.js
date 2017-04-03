b8189a6fee46354ef050cf42dc74c3d7225824c3
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2017-04-03 17:30:22 +0300

    Refactoring logger

--- src/core/stream.js
+++ src/core/stream.js
@@ -1,8 +1,8 @@
 // @flow
 
-import winston from 'winston';
 import { PassThrough } from 'stream';
 
+import { module } from '../utils/log-utils';
 import getFetch from '../fetch';
 import { decode, Decoder } from './decoder';
 import { wrap } from '../utils/stream-utils';
@@ -13,11 +13,14 @@ export default class Stream extends PassThrough {
   fetch: (string) => Promise<FetchResult>;
   channelId: string;
   decoder: Decoder;
+  log = module(this);
 
   terminated: boolean = false;
 
   constructor(backend: string, channelId: string) {
     super();
+
+    this.log('info', 'Initialize');
 
     this.channelId = channelId;
     this.fetch = getFetch(backend);
@@ -26,23 +29,26 @@ export default class Stream extends PassThrough {
   }
 
   stop() {
+    this.log('info', 'Stop');
     this.terminated = true;
     this.decoder.stop();
   }
 
   restart() {
+    this.log('info', 'Restart');
     this.emit('restart');
     this.decoder.stop();
   }
 
   _play() {
+    this.log('info', 'Play');
     this._fetchNowPlaying()
       .then(now => this._playNow(now))
       .catch(err => process.nextTick(() => this.emit('error', err)));
   }
 
   _playNow(now: FetchResult) {
-    winston.log('info', 'Playing now.', now);
+    this.log('info', 'Playing now "%s" from %d ms', now.title, now.offset);
     this.emit('title', now.title);
     this.decoder = decode(now.url, now.offset);
     this.decoder
@@ -57,11 +63,15 @@ export default class Stream extends PassThrough {
 
   _playNextOrEndIfTerminated() {
     if (!this.terminated) {
-      winston.log('info', 'Going to play next.');
+      this.log('info', 'Going to play next');
       this._play();
     } else {
-      winston.log('info', 'Going to end.');
+      this.log('info', 'Going to stop');
       this.end();
     }
+  }
+
+  toString(): string {
+    return `Stream("${this.channelId}")`;
   }
 }
